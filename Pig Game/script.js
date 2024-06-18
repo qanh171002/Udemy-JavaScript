14142f1a5a786df453d11f435e7711bfbe4525a0
qanh171002 <118640931+qanh171002@users.noreply.github.com>
2024-06-19 03:09:01 +0700

    increase max score

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -82,7 +82,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 50) {
       // finish the game
       playing = false;
       document
